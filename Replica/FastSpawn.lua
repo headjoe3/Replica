c021d6a580615e05eea5dc6e16da733009009790
Robert Tulley <johnathansherbert@gmail.com>
2021-05-20 15:43:27 -0600

    Use BindableFunction implementation instead

--- Replica/FastSpawn.lua
+++ Replica/FastSpawn.lua
@@ -1,19 +1,17 @@
--- Bloxx you, my FastSpawn(tm) is FasterSpawn than your spawn
--- DataBrain, 2019
+--!strict
 
-local FastestSpawn = Instance.new("BindableEvent")
-FastestSpawn.Event:Connect(function(callback, argsPointer)
-	callback(argsPointer())
-end)
+local BF = Instance.new('BindableFunction')
 
-local function createPointer(...)
-	local args = { ... }
-	return function()
-		return unpack(args)
-	end
+BF.OnInvoke = function(cb: any): any
+	return cb()
 end
 
-return function(func, ...)
-	assert(type(func) == "function", "Invalid arguments (function expected, got " .. typeof(func) .. ")")
-	FastestSpawn:Fire(func, createPointer(...))
+local function FastSpawn(func: any)
+	coroutine.resume(
+		coroutine.create(function()
+			BF:Invoke(func)
+		end)
+	)
 end
+
+return FastSpawn
