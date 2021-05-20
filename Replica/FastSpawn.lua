98035babb7250645ac00667a101331d9435a7add
Robert Tulley <johnathansherbert@gmail.com>
2021-05-20 15:45:24 -0600

    Add support for varargs

--- Replica/FastSpawn.lua
+++ Replica/FastSpawn.lua
@@ -6,10 +6,13 @@ BF.OnInvoke = function(cb: any): any
 	return cb()
 end
 
-local function FastSpawn(func: any)
+local function FastSpawn(func: any ...)
+	local args = { ... }
 	coroutine.resume(
 		coroutine.create(function()
-			BF:Invoke(func)
+			BF:Invoke(function()
+				cb(unpack(args))
+			end)
 		end)
 	)
 end
